3e0a888a7a6fd0d50df9acda6ce50388927b8b68
P94Vansh <vanshgambhirag@gmail.com>
2025-07-21 15:32:24 +0530

    checking api keys

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -4,6 +4,7 @@ import {Controller} from 'react-hook-form'
 import conf from '../conf/conf'
 
 export default function RTE({name,control,label,defaultValue=""}) {
+  console.log(conf.tinyMCEId)
   return (
     <div className='w-full'>
       {label && 
